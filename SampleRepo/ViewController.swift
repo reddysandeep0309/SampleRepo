4956765bdf7643820c5fa67a5c5861ce02cebbaa
Imran Mohammed <imran.mohammed3@ibm.com>
2018-12-17 12:19:48 +0530

    Initial Commit

---
+++ SampleRepo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleRepo
+//
+//  Created by Imran Imu on 17/12/18.
+//  Copyright © 2018 Prolifics. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
