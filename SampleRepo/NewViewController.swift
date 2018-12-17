1fb686f57da4e0754a6898a1c17ba35ef738c5f9
Imran Mohammed <imran.mohammed3@ibm.com>
2018-12-17 12:29:13 +0530

    modified

---
+++ SampleRepo/NewViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NewViewController.swift
+//  SampleRepo
+//
+//  Created by Imran Imu on 17/12/18.
+//  Copyright © 2018 Prolifics. All rights reserved.
+//
+
+import UIKit
+
+class NewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
